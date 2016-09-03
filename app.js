32a8a18ad8bb487dde30fe37effce4ccc54a4769
Michael Ehrlich <mic.ehrlich@gmail.com>
2016-09-03 12:40:38 +0000

    production version

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // connect to DB
-mongoose.connect(process.env.MONGODB_URI/* || process.env.MONGOLAB_URI*/);
+mongoose.connect(/*process.env.MONGODB_URI || */process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);
